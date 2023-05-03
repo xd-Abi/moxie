dac92e067cada5154a9219fcbde9eafa80b2925b
xd-Abi <abinayansureskumar@gmail.com>
2023-05-03 21:14:21 +0200

    Connecting to local mongo db and added docker compose for it

--- apis/signup/main.go
+++ apis/signup/main.go
@@ -6,17 +6,20 @@ import (
 	"github.com/xd-Abi/moxie/pkg/config"
 	"github.com/xd-Abi/moxie/pkg/constants"
 	"github.com/xd-Abi/moxie/pkg/logging"
+	"github.com/xd-Abi/moxie/pkg/mongodb"
 	"github.com/xd-Abi/moxie/pkg/network"
 	"github.com/xd-Abi/moxie/pkg/proto/hello"
 	"github.com/xd-Abi/moxie/pkg/proto/jwt"
 	"github.com/xd-Abi/moxie/pkg/proto/signup"
 	"github.com/xd-Abi/moxie/pkg/utils"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 var (
 	log          = logging.New()
 	jwtService   jwt.JwtServiceClient
 	helloService hello.HelloServiceClient
+	dbCollection *mongodb.MongoCollection
 )
 
 type SignUpServiceServer struct {
@@ -37,6 +40,12 @@ func (s *SignUpServiceServer) SignUp(ctx context.Context, request *signup.SignUp
 		return nil, constants.ErrPasswordEmpty
 	}
 
+	document := bson.D{
+		{Key: "name", Value: "John Doe"},
+		{Key: "email", Value: "john.doe@example.com"},
+	}
+	dbCollection.InsertOne(document)
+
 	return nil, nil
 }
 
@@ -44,6 +53,9 @@ func main() {
 	config.LoadEnvVariables(log)
 	jwtService = jwt.NewJwtServiceClient(network.NewGRPCClientConnection(config.GetUint("JWT_PORT"), log))
 	helloService = hello.NewHelloServiceClient(network.NewGRPCClientConnection(config.GetUint("HELLO_PORT"), log))
+
+	db := mongodb.Connect("mongodb://localhost:27001", "auth-api", "VpGdxrtBbzs0Zgxn5o", log)
+	dbCollection = db.GetCollection("auth", "users")
 
 	app := network.NewMicroServiceServer(config.GetUint("SIGN_UP_PORT"), log)
 	signup.RegisterSignUpServiceServer(app.InternalServer, &SignUpServiceServer{})
--- pkg/mongodb/mognodb.go
+++ pkg/mongodb/mognodb.go
@@ -7,6 +7,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 type MongoServer struct {
@@ -17,13 +18,24 @@ type MongoCollection struct {
 	internalCollection *mongo.Collection
 }
 
-func Connect(url string, log *logging.Log) *MongoServer {
-	options := options.Client().ApplyURI(url)
-	client, err := mongo.Connect(context.Background(), options)
+func Connect(url string, username string, password string, log *logging.Log) *MongoServer {
+	opt := options.Client().ApplyURI(url)
+	opt.Auth = &options.Credential{
+		Username: username,
+		Password: password,
+	}
+	client, err := mongo.Connect(context.Background(), opt)
 
 	if err != nil {
 		log.Fatal("Failed to connect to mongo server: %v", err)
 	}
+
+	err = client.Ping(context.Background(), readpref.Primary())
+	if err != nil {
+		log.Fatal("Failed to connect to mongo server: %v", err)
+	}
+
+	log.Info("Connected to database")
 
 	return &MongoServer{
 		internalClient: client,
@@ -32,6 +44,7 @@ func Connect(url string, log *logging.Log) *MongoServer {
 
 func (s *MongoServer) GetCollection(database string, collection string) *MongoCollection {
 	col := s.internalClient.Database(database).Collection(collection)
+
 	return &MongoCollection{
 		internalCollection: col,
 	}
