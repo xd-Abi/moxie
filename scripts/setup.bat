d69dd19dd3ac0610b754dd17810dcfb90c8afc9b
xd-Abi <abinayansureskumar@gmail.com>
2023-05-03 18:45:14 +0200

    Added proto buffers for token micro service

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -1,6 +1,6 @@
 @echo off
 pushd %~dp0\..\
 echo Generating proto buffers...
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative pkg/proto/hello/hello.proto
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative pkg/proto/hello/hello.proto pkg/proto/token/token.proto
 popd
 PAUSE
