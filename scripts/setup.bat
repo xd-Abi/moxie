8d0119d681c1960cbeb7a499ab43cfe738e6f85f
xd-Abi <abinayansureskumar@gmail.com>
2023-05-03 22:05:02 +0200

    Added proto buffers for changing password service

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -1,6 +1,10 @@
 @echo off
 pushd %~dp0\..\
 echo Generating proto buffers...
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative pkg/proto/hello/hello.proto pkg/proto/jwt/jwt.proto pkg/proto/signup/signup.proto
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative pkg/proto/change-password/change-password.proto 
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative pkg/proto/hello/hello.proto 
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative pkg/proto/jwt/jwt.proto 
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative pkg/proto/signup/signup.proto
+echo Proto generation completed.
 popd
 PAUSE
