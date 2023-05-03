d553e2d845109179dd5f9aaf939bbb85a3283899
xd-Abi <abinayansureskumar@gmail.com>
2023-05-03 23:47:35 +0200

    Added login protocol buffer

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -4,6 +4,7 @@ echo Generating proto buffers...
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative pkg/proto/change-password/change-password.proto 
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative pkg/proto/hello/hello.proto 
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative pkg/proto/jwt/jwt.proto 
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative pkg/proto/login/login.proto 
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative pkg/proto/signup/signup.proto
 echo Proto generation completed.
 popd
