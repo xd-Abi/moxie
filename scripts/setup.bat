dcd8a57c7cf90f3045345531b361cc6d9f593ab7
xd-Abi <abinayansureskumar@gmail.com>
2023-05-04 22:07:31 +0200

    Added protocol buffers for profile service

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -3,6 +3,7 @@ pushd %~dp0\..\
 echo Generating proto buffers...
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative pkg/proto/auth/auth.proto
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative pkg/proto/health/health.proto
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative pkg/proto/profile/profile.proto
 echo Proto generation completed.
 popd
 PAUSE
